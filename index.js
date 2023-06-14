d0e98cb9c6978e0e858259045490c1826b4980e0
Sanzid 2.0 <sanzid.official@gmail.comm>
2023-06-15 04:36:57 +0600

    verify instructor

--- index.js
+++ index.js
@@ -81,6 +81,16 @@ async function run() {
       const result = {admin: user.role === 'admin'};
       res.send(result);
     })
+    app.get('/users/instructor/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      if(req.decoded.email !== email){
+        return res.send({instructor: false})
+      }
+      const query = {email: email};
+      const user = await userCollection.findOne(query);
+      const result = {instructor: user.role === 'instructor'};
+      res.send(result);
+    })
     app.patch('/users/admin/:id', async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
