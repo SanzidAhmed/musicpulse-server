7450f5763a5f54d3872f52be21971b4bab38ac44
Sanzid 2.0 <sanzid.official@gmail.comm>
2023-06-15 03:18:58 +0600

    admin verify section done

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ app.use(cors());
 app.use(express.json());
 
 
-
 const verifyJWT = (req, res, next) => {
   const authorization = req.headers.authorization;
   if (!authorization) {
@@ -54,7 +53,7 @@ async function run() {
 
     app.post('/jwt', (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1hr' });
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
       res.send({ token })
     })
     // user apis
@@ -70,6 +69,16 @@ async function run() {
         return res.send({ message: "User already exists" })
       }
       const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+    app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      if(req.decoded.email !== email){
+        return res.send({admin: false})
+      }
+      const query = {email: email};
+      const user = await userCollection.findOne(query);
+      const result = {admin: user.role === 'admin'};
       res.send(result);
     })
     app.patch('/users/admin/:id', async (req, res) => {
